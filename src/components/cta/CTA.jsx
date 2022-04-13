bdd58dc290533242549dced52b82180fd8039751
Joshua McGehee <mcgehee.joshua@gmail.com>
2022-04-13 16:52:39 -0600

    Added folders/files for all components

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import './cta.css';
+import React from 'react'
+import './cta.csss'
 
 const CTA = () => {
-    return (
-        <div>
-            CTA
-        </div>
-    )
+  return (
+    <div>CTA</div>
+  )
 }
 
 export default CTA
